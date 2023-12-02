14339bd0772398e18aae83b5a0683a6ceb80e44b
mpa12 <maksim.28.05@mail.ru>
2023-12-02 23:31:38 +1000

    #5 Страница с услугами

--- stroyinvest/attendance/views.py
+++ stroyinvest/attendance/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'services/services.html')
+
+def view(request, service_id):
+    return render(request, 'services/service.html')
