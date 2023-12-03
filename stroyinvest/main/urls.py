e7675ed89d1b98e3f12966c602946b4918f4bfcc
mpa12 <maksim.28.05@mail.ru>
2023-12-03 17:38:04 +1000

    #10 Страница "О нас"

--- stroyinvest/main/urls.py
+++ stroyinvest/main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('documents', views.documents, name='documents'),
     path('contacts', views.contacts, name='contacts'),
+    path('about-us', views.about_us, name='about-us'),
     path('products', include('products.urls')),
     path('services', include('attendance.urls')),
     path('news', include('news.urls')),
