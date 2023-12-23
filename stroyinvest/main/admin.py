6269c08037bd4e0eefd02702edb0064016f79fea
Mikhail Karpukhin <mishan2003@bk.ru>
2023-12-24 03:38:23 +1000

    Настройки админки

--- stroyinvest/main/admin.py
+++ stroyinvest/main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
+admin.site.site_header = 'ООО «СТРОЙИНВЕСТ»'
+admin.site.site_title = 'ООО «СТРОЙИНВЕСТ»'
+admin.site.index_title = 'ООО «СТРОЙИНВЕСТ»'
+
 admin.site.unregister(User)
 admin.site.unregister(Group)
