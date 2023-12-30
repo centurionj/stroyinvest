d3ac24d62dafeeb82d1ca78d5be5c16f7b94743d
Mikhail Karpukhin <mishan2003@bk.ru>
2023-12-31 04:03:44 +1000

    Доработка докера, добавление конфига гуникорн

--- stroyinvest/deploy/run.sh
+++ stroyinvest/deploy/run.sh
@@ -6,7 +6,7 @@ python "/app/manage.py" collectstatic --noinput
 
 python "/app/manage.py" loaddata "service/fixtures/icons_fixture.json"
 
-gunicorn "stroyinvest.wsgi:application" --bind 0.0.0.0:8000 &
+gunicorn -c "./deploy/gunicorn.conf.py" stroyinvest.wsgi:application &
 
 celery -A "stroyinvest" beat -l info &
 
